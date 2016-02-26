ea6e7a7830628dcf4dc7a1b3628a036304478143
jmatyja <januszmatyja@gmail.com>
2016-02-26 12:41:32 +0000

    login page

--- fut/connector.js
+++ fut/connector.js
@@ -7,23 +7,39 @@ export default function connectToWebApp(config) {
   var serverData = {};
 
   var options = {
+    uri: 'https://www.easports.com/uk/fifa/ultimate-team/web-app',
     timeout: 10000,
     headers: {
-      'User-Agent': 'Mozilla/6.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0',
-      'Host': 'signin.ea.com'
+      'User-Agent': 'Mozilla/6.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0'
     },
-    method: 'GET',
     jar: rp.jar(new FileCookieStore('./fut/cookies.json')),
-    followAllRedirects :true
+    followAllRedirects :true,
+    resolveWithFullResponse: true
   };
   return new Promise((resolve, reject) => {
     vasync.waterfall([
       callback => {
-        rp(Object.assign(options), {})
-          .then($ => $)
-          .catch(error => callback(error))
+        rp(Object.assign(options, {jar: true}))
+          .then(response => callback(null, response.request.uri.href))
+          .catch(error => callback(error));
       },
-      (extra, callback) => { callback();}
+      (loginUrl, callback) => { 
+        rp(Object.assign(options, 
+          {
+            uri: loginUrl,
+            headers: Object.assign(options.headers, {
+              jar: true,
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Referer': loginUrl,
+              'Host': 'signin.ea.com'
+            }),
+            method: "POST",
+            form: {email: 'psjmone@gmail.com', password: 'Klebuzsek2', remeberMe: 'on', _eventId: 'submit', gCaptchaResponse: ''}
+          })
+        )
+          .then(response => {console.log(response);})
+          .catch(error => callback(error));
+      }
     ], (error) => {
       error ? reject(error) : resolve('doneabc');
     });
